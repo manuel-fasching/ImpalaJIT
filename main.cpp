2c1bd58d9cf18ab0df0daf45fc3e4587d7bc8247
Manuel Fasching <manuel.fasching@elpt.de>
2016-10-17 16:52:54 +0200

    Added ttc example application

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "libtcc.h"
+
+int main(int argc, char *argv[])
+{
+
+    TCCState *s;
+    unsigned long val;
+    char* src;
+    char cwd[1024];
+
+    s = tcc_new();
+    if (!s) {
+        fprintf(stderr, "Could not create tcc state\n");
+        exit(1);
+    }
+
+    tcc_set_output_type(s, TCC_OUTPUT_MEMORY);
+
+    tcc_add_file(s, argv[1]);
+
+    tcc_run(s, 0, NULL);
+
+    exit(0);
 }
