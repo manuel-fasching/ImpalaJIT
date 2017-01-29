27a471b953977b7f63f469ec99f4c8b02ff7e689
Manuel Fasching <manuel.fasching@elpt.de>
2017-01-29 13:33:56 +0100

    added complex conditional test cases

---
+++ tests/conditionals_complex_1.cc
@@ -0,0 +1,38 @@
+//
+// Created by manuel on 29.01.17.
+//
+
+#include "impalajit.hh"
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    setenv("IMPALA_FILE","../../tests/impala_files/conditional_complex_1.impala", 1);
+
+    impalajit::Compiler compiler;
+    dasm_gen_func function = compiler.compile();
+
+    assert(function(1.0, 1.0, 1.0, 1.0) == 1.0);
+    assert(function(1.0, 1.0, 1.0, 0.0) == 1.0);
+    assert(function(1.0, 1.0, 0.0, 1.0) == 1.0);
+    assert(function(1.0, 1.0, 0.0, 0.0) == 1.0);
+    assert(function(1.0, 0.0, 1.0, 1.0) == 1.0);
+    assert(function(1.0, 0.0, 1.0, 0.0) == 2.0);
+    assert(function(1.0, 0.0, 0.0, 1.0) == 2.0);
+    assert(function(1.0, 0.0, 0.0, 0.0) == 2.0);
+    assert(function(0.0, 1.0, 1.0, 1.0) == 2.0);
+    assert(function(0.0, 1.0, 1.0, 0.0) == 2.0);
+    assert(function(0.0, 1.0, 0.0, 1.0) == 2.0);
+    assert(function(0.0, 1.0, 0.0, 0.0) == 2.0);
+    assert(function(0.0, 0.0, 1.0, 1.0) == 2.0);
+    assert(function(0.0, 0.0, 1.0, 0.0) == 2.0);
+    assert(function(0.0, 0.0, 0.0, 1.0) == 2.0);
+    assert(function(0.0, 0.0, 0.0, 0.0) == 2.0);
+
+    return 0;
+}
