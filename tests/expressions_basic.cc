ca41679923dc91da1fb6a9a39f1d7dafc6d8413b
Manuel Fasching <manuel.fasching@elpt.de>
2017-02-03 15:11:31 +0100

    Added basic expression test

---
+++ tests/expressions_basic.cc
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2017 Manuel Fasching <manuel.fasching@tum.de>
+ * Distributed under the MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "impalajit.hh"
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include <defines.hh>
+#include <math.h>
+#include <limits>
+using namespace std;
+
+
+double reference_function(double x, double y){
+    return pow(sqrt(3.01*x+(-y+12.65)), 2.54)/3.4331;
+}
+
+bool double_equals(double a, double b, double epsilon = pow(10, -12))
+{
+    if(a<b) {
+        return b - a < epsilon;
+    }
+    else {
+        return a - b < epsilon;
+    }
+}
+
+
+int main(int argc, char** argv) {
+
+    ofstream configFile;
+    configFile.open(CONFIG_FILE_PATH);
+    configFile << "../../tests/impala_files/expression_basic.impala;";
+    configFile.close();
+
+    impalajit::Compiler compiler(CONFIG_FILE_PATH);
+    compiler.compile();
+    dasm_gen_func function = compiler.getFunction("expression_basic");
+    assert(double_equals(function(2.54, -4.21), reference_function(2.54, -4.21)));
+    assert(double_equals(function(212.421, -232.22), reference_function(212.421, -232.22)));
+    return 0;
+}
+
